8a415a7eabd877a7846f55fa34bfa8d98f7f969b
sujith102007 <sujith.2140@gmail.com>
2026-09-16 11:59:59 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,27 +27,31 @@ async def test_project(dut):
     dut._log.info("Testing Half Adder")
 
     # Test 00: A=0, B=0
-    dut.ui_in.value = 0
+    dut.ui_in.value = 0b00000000
     await ClockCycles(dut.clk, 1)
 
+    # SUM = 0, CARRY = 0
     assert dut.uo_out.value & 0x03 == 0b00
 
     # Test 01: A=0, B=1
-    dut.ui_in.value = 1
+    dut.ui_in.value = 0b00000001
     await ClockCycles(dut.clk, 1)
 
+    # SUM = 1, CARRY = 0
     assert dut.uo_out.value & 0x03 == 0b01
 
     # Test 10: A=1, B=0
-    dut.ui_in.value = 1 << 1
+    dut.ui_in.value = 0b00000010
     await ClockCycles(dut.clk, 1)
 
+    # SUM = 1, CARRY = 0
     assert dut.uo_out.value & 0x03 == 0b01
 
     # Test 11: A=1, B=1
-    dut.ui_in.value = 3
+    dut.ui_in.value = 0b00000011
     await ClockCycles(dut.clk, 1)
 
+    # SUM = 0, CARRY = 1
     assert dut.uo_out.value & 0x03 == 0b10
 
     dut._log.info("Half Adder test completed successfully!")
