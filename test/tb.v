6f6bd052b3ba2f4c4750f730f6cc8ec41ddd96e1
sujith102007 <sujith.2140@gmail.com>
2026-09-16 12:01:28 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -34,7 +34,7 @@ module tb ();
 `endif
 
     // Replace tt_um_example with your module name
-    tt_um_half_adder (
+    tt_um_example user_project (
 
         // Include power ports for the Gate Level test
 `ifdef GL_TEST
